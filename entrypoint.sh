3e51768b68ce7510146db8141ad83f5458594a7f
Dan Stewart <git@mail.danstewart.dev>
2023-11-19 14:22:30 +0000

    Fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
 
-cd phoenix || { echo "phoenix directory not found"; exit 1; }
 
 # Wait until db is ready
-until mysql -hdb -uroot -p${MARIADB_ROOT_PASSWORD} --silent -e "SELECT 1;" >/dev/null; do
+until mysql -hdb -uroot -p"${MARIADB_ROOT_PASSWORD}" --silent -e "SELECT 1;" >/dev/null; do
     echo "Waiting for database to start"
     sleep 5
 done
 
-# function does_db_exist() {
-#     database_name="$1"
-#     docker-compose exec db mysql -h127.0.0.1 -uroot -p${MARIADB_ROOT_PASSWORD} -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${database_name}'"
-#     return 0
-# }
+# Create the database if it doesn't already exist
+docker-compose exec db mariadb -h127.0.0.1 -uroot -p"${MARIADB_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS '${DATABASE}'"
 
-# # Create, migrate, and seed database if it doesn't exist.
-# if [[ does_db_exist ${DATABASE} ]]; then
-#   echo "Database ${DATABASE} does not exist. Creating..."
-#   mix ecto.create
-#   mix ecto.migrate
-#   mix run priv/repo/seeds.exs
-#   echo "Database $DATABASE created."
-# fi
+# Apply migrations
+mix ecto.create
+mix ecto.migrate
+mix run priv/repo/seeds.exs
 
+# Build assets
+cd assets && npm rebuild node-sass && cd ..
+
+# Start server
 exec mix phx.server
